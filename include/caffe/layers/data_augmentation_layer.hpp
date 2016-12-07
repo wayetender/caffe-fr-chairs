e56ad6eb09641d4c7d3c145fd7a4fb3a89a7afe4
Lucas Waye <wayetender@gmail.com>
2016-12-06 19:20:34 -0500

    add missing functions for cpu build gpu stub, updating docker file for cpu build

--- include/caffe/layers/data_augmentation_layer.hpp
+++ include/caffe/layers/data_augmentation_layer.hpp
@@ -29,7 +29,11 @@ class DataAugmentationLayer : public Layer<Dtype> {
       const vector<Blob<Dtype>*>& top);
   virtual void Backward_cpu(const vector<Blob<Dtype>*>& top,
       const vector<bool>& propagate_down, const vector<Blob<Dtype>*>& bottom)  { LOG(FATAL) << "DataAugmentationLayer does not do backward"; }
-
+  virtual void Forward_gpu(const vector<Blob<Dtype>*>& bottom, 
+    const vector<Blob<Dtype>*>& top);
+  virtual void Backward_gpu(const vector<Blob<Dtype>*>& top, 
+    const vector<bool>& propagate_down, \
+    const vector<Blob<Dtype>*>& bottom);
       
   virtual void generate_spatial_coeffs(const AugmentationParameter& aug, AugmentationCoeff& coeff, Dtype discount_coeff = 1);
   virtual void generate_chromatic_coeffs(const AugmentationParameter& aug, AugmentationCoeff& coeff, Dtype discount_coeff = 1);
